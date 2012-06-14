845928b1b5b28ccf32c8a57e73737e841e156403
freemanoid <freemanoid321@gmail.com>
2012-06-14 11:51:48 +0300

    Fix point scaling in drawdialog.

--- drawdialog.cpp
+++ drawdialog.cpp
@@ -61,100 +61,6 @@ QSize DrawDialog::sizeHint() const
 
 void DrawDialog::paintEvent(QPaintEvent *e)
 {
-//    QPainter painter(this);
-//    painter.translate(indent, indent);
-//    painter.translate(m_size.width(), m_size.height());
-//    painter.setRenderHint(QPainter::Antialiasing);
-//    //coordinate plain painting and set ordinary coordinate plain except of toQtY()
-//    painter.setPen(Qt::red);//oY
-//    painter.drawLine(QPoint(0, toQtY(-m_size.height())), QPoint(0, toQtY(m_size.height())));//axis
-//    painter.drawLine(QPoint(0, toQtY(m_size.height())), QPoint(-3, toQtY(m_size.height() - 5)));//arrow
-//    painter.drawLine(QPoint(0, toQtY(m_size.height())), QPoint(3, toQtY(m_size.height() - 5)));//arrow
-//    painter.drawStaticText(QPoint(-5, toQtY(m_size.height() + 20)), QStaticText("Y"));
-//    painter.setPen(Qt::blue);//oX
-//    painter.drawLine(QPoint(-m_size.width(), toQtY(0)), QPoint(m_size.width(), toQtY(0)));//axis
-//    painter.drawLine(QPoint(m_size.width(), toQtY(0)), QPoint(m_size.width() - 5, toQtY(-3)));//arrow
-//    painter.drawLine(QPoint(m_size.width(), toQtY(0)), QPoint(m_size.width() - 5, toQtY(3)));//arrow
-//    painter.drawStaticText(QPoint(m_size.width() + 5, toQtY(10)), QStaticText("X"));
-//    painter.setClipRect(QRect(QPoint(-m_size.width(), toQtY(m_size.height())),
-//                              QPoint(m_size.width(), toQtY(-m_size.height()))));
-//    //grid
-//    painter.setPen(Qt::lightGray);
-//    quint16 gridStep = 10 * m_scale;
-//    for(quint16 itr = 1; gridStep * itr < m_size.width(); itr++)//vertical
-//    {
-//        painter.drawLine(QPoint(itr * gridStep, toQtY(-m_size.height())),
-//                         QPoint(itr * gridStep, toQtY(m_size.height())));
-//        painter.drawLine(QPoint(-itr * gridStep, toQtY(-m_size.height())),
-//                         QPoint(-itr * gridStep, toQtY(m_size.height())));
-//    }
-//    for(quint16 itr = 1; gridStep * itr < m_size.height(); itr++)//horizontal
-//    {
-//        painter.drawLine(QPoint(-m_size.width(), toQtY(itr * gridStep)),
-//                         QPoint(m_size.width(), toQtY(itr * gridStep)));
-//        painter.drawLine(QPoint(-m_size.width(), toQtY(-itr * gridStep)),
-//                         QPoint(m_size.width(), toQtY(-itr * gridStep)));
-//    }
-//    //end of coordinate plain painting
-//    painter.setPen(m_linesColor);
-
-//    //text font
-//    painter.setFont(QFont(QApplication::font().family(), 7));
-//    foreach(const GraphicElement *itr, m_whatToDrawList)
-//        switch(itr->m_ElementType)
-//        {
-//        case GraphicElement::Line:
-//            const DrawLine *Line = dynamic_cast<const DrawLine *>(itr);
-//            if(Line->a == 0 && Line->b == 0)
-//            {
-//                qDebug() << "a == 0 and b == 0 wtf?? it will be deleted now";
-//                m_whatToDrawList.removeOne(const_cast<GraphicElement *>(itr));
-//                //такого не может быть
-//            }
-//            else if(Line->a == 0)
-//            {
-//                painter.setPen(m_linesColor);
-//                painter.drawLine(QPointF(-m_size.width(),
-//                                        toQtY((-1) * Line->c / Line->b * m_scale)),
-//                                 QPointF(m_size.width(),
-//                                        toQtY((-1) * Line->c / Line->b * m_scale)));
-//                painter.setPen(m_textColor);
-//                painter.drawStaticText(QPointF(0 - 20,
-//                                              toQtY((-1) * Line->c / Line->b * m_scale + 9)),
-//                                       QStaticText(QString::number(((-1) * Line->c / Line->b), 'f', 1)));
-//            }
-//            else if(Line->b == 0)
-//            {
-//                painter.setPen(m_linesColor);
-//                painter.drawLine(QPointF((-1) * Line->c / Line->a * m_scale,
-//                                        toQtY(-m_size.height())),
-//                                 QPointF((-1) * Line->c / Line->a * m_scale,
-//                                        toQtY(m_size.height())));
-//                painter.setPen(m_textColor);
-//                painter.drawStaticText(QPointF((-1) * Line->c / Line->a * m_scale - 20,
-//                                              toQtY(0 + 11)),
-//                                       QStaticText(QString::number(((-1) * Line->c / Line->a), 'f', 1)));
-//            }
-//            else
-//            {
-//                painter.setPen(m_linesColor);
-////                painter.drawLine(QPointF((-1) * (Line->c + Line->b * m_size.height()) / Line->a,
-////                                         toQtY(m_size.height())),
-////                                 QPointF((-1) * (Line->c * (2 * m_scale - 1) + Line->b * -m_size.height()) / Line->a,
-////                                         toQtY(-m_size.height()))); //the same but in abbreaviated form
-//                painter.drawLine(QPointF((-1) * (Line->c + Line->b * m_size.height()) / Line->a,
-//                                         toQtY(m_size.height())),
-//                                 QPointF((-1) * (Line->c + Line->b * -m_size.height()) / Line->a * m_scale-
-//                                         (-1) * (Line->c + Line->b * m_size.height()) / Line->a * (1 - m_scale),
-//                                         toQtY(-m_size.height())));
-//                painter.setPen(m_textColor);
-//                painter.drawText(QPointF(0, toQtY((-1) * Line->c / Line->b * m_scale)),
-//                                 QString::number((-1) * Line->c / Line->b, 'f', 1));
-//                painter.drawText(QPointF((-1) * Line->c / Line->a * m_scale, toQtY(0)),
-//                                 QString::number((-1) * Line->c / Line->a, 'f', 1));
-//            }
-//            break;
-//        }
     QPainter painter(this);
     painter.translate(indent, indent);
     painter.translate(m_size.width(), m_size.height());
@@ -258,10 +164,13 @@ void DrawDialog::paintEvent(QPaintEvent *e)
         }
     if(m_firstClickOccured)
     {
+        painter.restore();
+        painter.save();
         QBrush previousBrush = painter.brush();
         painter.setBrush(QBrush(Qt::yellow));
         painter.drawEllipse(m_userClickPoint, 3, 3);
         painter.setBrush(previousBrush);
+        painter.scale(m_scale, m_scale);
     }
 
     painter.restore();//it generaly hasn't any sense but it needs to avoid multiply warnings about not restored painter save
